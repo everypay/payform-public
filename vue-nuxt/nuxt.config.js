9243654ab9f970fef33555ef108d836c738840a1
George <gleverypay@users.noreply.github.com>
2020-11-03 13:09:59 +0200

    adding vue-nuxt example

---
+++ vue-nuxt/nuxt.config.js
@@ -0,0 +1,48 @@
+export default {
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'vue-nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {
+        src: 'https://sandbox-js.everypay.gr/v3'
+      }
+    ],
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  }
+}
---
+++ vue-nuxt/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex items-center justify-center md:justify-start">
+    <div class="w-full max-w-sm">
+      <div id="pay-form"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+  methods: {
+    handleResponse(r) {
+      if (r.onLoad) {
+        var elementExists =  document.getElementById('loader');
+        if (typeof(elementExists) != 'undefined' && elementExists != null)
+        {
+          document.getElementById('loader').remove();
+        }
+      }
+      console.log(r.data)
+    }
+  },
+
+  mounted() {
+
+    var payload = {
+      pk: 'pk_vI1tWSHzaskrVBobzcqvwk0uYAWXR8BP',
+      amount: 50,
+      locale: 'el',
+      txnType: 'tds',
+      theme:'default',
+    }
+
+    everypay.payform(payload,this.handleResponse);
+  }
+}
+</script>
